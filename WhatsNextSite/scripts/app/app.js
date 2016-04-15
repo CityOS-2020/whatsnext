26b72105464ca8a25f34c7a2137ba7124efaa39b
corp\ib250092 <irhadba@maestralsolutions.com>
2016-04-15 20:58:33 +0200

    Initial commit.

---
+++ WhatsNextSite/scripts/app/app.js
@@ -0,0 +1,11 @@
+﻿var whatsNextApp = angular.module("whatsNextApp", ["ngRoute", "ngAnimate", "ui.bootstrap"]);
+
+whatsNextApp.config(function ($routeProvider) {
+
+    $routeProvider
+        .when("/content", {
+            templateUrl: "scripts/app/views/content.html",
+            controller: "contentController"
+        })
+        .otherwise({ redirectTo: "/content" });
+});
---
+++ WhatsNextSite/scripts/app/controllers/contentController.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict';
+    angular.module('whatsNextApp').controller('contentController', contentController);
+    contentController.$inject = ['$scope', 'dataService']
+    function contentController($scope, dataService) {
+
+        dataService.getAll('content').then(function (data) {
+            $scope.content = data;
+        });
+    };
+}());
