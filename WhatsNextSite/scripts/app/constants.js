1f4ba31720f1eea80576769875419ab8cac7d31b
corp\ib250092 <irhadba@maestralsolutions.com>
2016-04-17 06:42:03 +0200

    website endpoint switched to azure

--- WhatsNextSite/scripts/app/constants.js
+++ WhatsNextSite/scripts/app/constants.js
@@ -2,6 +2,6 @@
     'use strict';
 
     angular.module('whatsNextApp')
-    //.constant('apiUrl', 'http://whatsnextapi.azurewebsites.net/api/');
-    .constant('apiUrl', 'http://localhost:19150/api/');
+    .constant('apiUrl', 'http://whatsnextapi.azurewebsites.net/api/');
+    //.constant('apiUrl', 'http://localhost:19150/api/');
 })();
